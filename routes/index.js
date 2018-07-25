3642e39fffddeed883f49118edad0fc03284a8be
Reut Levy <settrue0@gmail.com>
2018-07-25 14:23:54 +0300

    all users mongo

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,10 @@ import User from '../models/user';
 const router = Router();
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res, next) => {
   // res.render('index', { title: 'Express!' });
-  const userDetails = {
-    userName: "String",
-    password: "String",
-    typePermission: "String"
-  };
-  const newUser = await User.create(userDetails);
 
-  res.send(newUser);
+  res.send("Hello");
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+import User from '../models/user';
+
+const router = Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async(req, res, next) => {
+  const userDetails = {
+    userName: "String1",
+    password: "String1",
+    typePermission: "String1"
+  };
+  const newUser = await User.create(userDetails);
+
+  res.send(newUser);
+});
+
+
+router.get('/allUsers', async(req, res, next) => {
+  const allUsers = await User.find();
+  if(allUsers){
+    res.send(allUsers);
+  }else{
+    res.send("error");
+  }
 });
 
 module.exports = router;
